bcad2a4cbae64bdd5e7967b479f43eea64d04fe2
ChaoticSalad <olerator97@gmail.com>
2020-08-14 17:42:03 +0300

    Initial

---
+++ hw6/hw6ex3/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Переделать программу Пример использования коллекций для решения следующих задач:
+//а) Подсчитать количество студентов учащихся на 5 и 6 курсах;
+//б) подсчитать сколько студентов в возрасте от 18 до 20 лет на каком курсе учатся(*частотный массив);
+//в) отсортировать список по возрасту студента;
+//г) * отсортировать список по курсу и возрасту студента;
+//      Черников Олег
+namespace hw6ex3
+{
+    class Student
+    {
+        public string lastName;
+        public string firstName;
+        public string university;
+        public string faculty;
+        public int course;
+        public string department;
+        public int group;
+        public string city;
+        public int age;
+        public Student(string firstName, string lastName, string university, string faculty, string department, int age, int course, int group, string city)
+        {
+            this.lastName = lastName;
+            this.firstName = firstName;
+            this.university = university;
+            this.faculty = faculty;
+            this.department = department;
+            this.age = age;
+            this.course = course;
+            this.group = group;
+            this.city = city;
+        }
+    }
+    class Program
+    {
+
+        static void CompareAgeAndCourse(ref List<Student> list)
+        {
+            list.Sort(delegate (Student st1, Student st2) { return String.Compare(st1.age.ToString(), st2.age.ToString()); });
+            list.Sort(delegate (Student st1, Student st2) { return String.Compare(st1.course.ToString(), st2.course.ToString()); });
+        }
+
+        static void Main(string[] args)
+        {
+            int bakalavr = 0;
+            int magistr = 0;
+            int fifthCourse = 0;
+            int sixthCourse = 0;
+            string courseFreq = String.Empty;
+            Dictionary<int, int> course = new Dictionary<int, int>();
+            List<Student> students = new List<Student>();
+            DateTime dt = DateTime.Now;
+            StreamReader sr = new StreamReader("students_6.csv");
+            while (!sr.EndOfStream)
+            {
+                try
+                {
+                    string[] s = sr.ReadLine().Split(',');
+                    students.Add(new Student(s[0], s[1], s[2], s[3], s[4], int.Parse(s[5]), int.Parse(s[6]), int.Parse(s[7]), s[8]));
+                    if (int.Parse(s[6]) < 5) bakalavr++; else magistr++;
+                    if (int.Parse(s[6]) == 5) fifthCourse++;
+                    if (int.Parse(s[6]) == 6) sixthCourse++;
+                    if (int.Parse(s[5]) >= 18 && int.Parse(s[5]) <= 20)
+                    {
+                        if (course.ContainsKey(int.Parse(s[6])))
+                            course[int.Parse(s[6])]++;
+                        else
+                            course[int.Parse(s[6])] = 1;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Error! ESC - to stop");
+                    if (Console.ReadKey().Key == ConsoleKey.Escape) return;
+                }
+            }
+            sr.Close();
+
+            students.Sort(delegate (Student st1, Student st2) { return String.Compare(st1.firstName, st2.firstName);  });
+            foreach (var v in students)
+                Console.WriteLine(v.firstName);
+
+            Console.WriteLine(new string('-', 30));
+
+            Console.WriteLine("Sort by age: ");
+            students.Sort(delegate (Student st1, Student st2) { return String.Compare(st1.age.ToString(), st2.age.ToString()); });
+            foreach (var v in students)
+                Console.WriteLine(v.firstName);
+
+            Console.WriteLine(new string('-', 30));
+
+            Console.WriteLine("Sort by course and age: ");
+            CompareAgeAndCourse(ref students);
+            foreach (var v in students)
+                Console.WriteLine(v.firstName);
+
+            Console.WriteLine(new string('-', 30));
+
+            Console.WriteLine($"Number of students: {students.Count}");
+            Console.WriteLine($"Masters: {magistr}");
+            Console.WriteLine($"Bachelors: {bakalavr}");
+            Console.WriteLine($"Students on fifth course: {fifthCourse}");
+            Console.WriteLine($"Students on sixth course: {sixthCourse}");
+            foreach (var pair in course)
+                courseFreq += $"On {pair.Key} course {pair.Value} students\n";
+            Console.WriteLine(courseFreq);
+            Console.WriteLine(DateTime.Now - dt);
+            Console.ReadKey();
+
+        }
+    }
+
+}
---
+++ hw6/hw6ex4/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//** Считайте файл различными способами.Смотрите “Пример записи файла различными способами”. 
+//    Создайте методы, которые возвращают массив byte (FileStream, BufferedStream), строку для StreamReader и массив int для BinaryReader.
+//    Черников Олег
+namespace hw6ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            long kb = 1024;
+            long mb = 1024 * kb;
+            long size = mb;
+            Console.WriteLine("Запишем файлы при помощи разных потоков:");
+            Console.WriteLine("FileStream. Milliseconds:{0}", WriteFileStream("bigdata0.bin", size, random));
+            Console.WriteLine("BinaryStream. Milliseconds:{0}", WriteBinaryStream("bigdata1.bin", size, random));
+            Console.WriteLine("StreamWriter. Milliseconds:{0}", WriteStreamWriter("bigdata2.bin", size, random));
+            Console.WriteLine("BufferedStream. Milliseconds:{0}", WriteBufferedStream("bigdata3.bin", size, random));
+
+            Console.WriteLine("Прочтём файлы при помощи разных потоков:");
+            byte[] bytesFromFileStream = ReadFileStream("bigdata0.bin");
+            int[] integersFromBinatyStream = ReadBinaryStream("bigdata1.bin");
+            string stringFromSreamReader = ReadStreamReader("bigdata2.bin");
+            byte[] bytesFromBufferedStream = ReadBufferedStream("bigdata3.bin");
+
+            Console.ReadKey();
+        }
+
+        static long WriteFileStream(string filename, long size, Random random)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            FileStream fs = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            for (int i = 0; i < size; i++)
+                fs.WriteByte(Convert.ToByte(random.Next(255)));
+            fs.Close();
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        static byte[] ReadFileStream(string filename)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            byte[] byteArray = new byte[fs.Length];
+            for (int i = 0; i < fs.Length; i++)
+                byteArray[i] = (byte)fs.ReadByte();
+            fs.Close();
+            return byteArray;
+        }
+
+        static long WriteBinaryStream(string filename, long size, Random random)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            FileStream fs = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            for (int i = 0; i < size; i++)
+                bw.Write(Convert.ToByte(random.Next(255)));
+            fs.Close();
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        static int[] ReadBinaryStream(string filename)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            int[] intArr = new int[fs.Length / 4];
+            BinaryReader br = new BinaryReader(fs);
+            for (int i = 0; i < fs.Length / 4; i++)
+                intArr[i] = br.ReadInt32();
+            fs.Close();
+            return intArr;
+        }
+
+        static long WriteStreamWriter(string filename, long size, Random random)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            FileStream fs = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            for (int i = 0; i < size; i++)
+                sw.Write(random.Next(9));
+            fs.Close();
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        static string ReadStreamReader(string filename)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string result = sr.ReadToEnd();
+            fs.Close();
+            return result;
+        }
+
+        static long WriteBufferedStream(string filename, long size, Random random)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            FileStream fs = new FileStream(filename, FileMode.Create, FileAccess.Write);
+            int countPart = 4; //number of parts
+            int bufsize = (int)(size / countPart);
+            byte[] buffer = new byte[size];
+            for (int i = 0; i < buffer.Length; i++)
+                buffer[i] = Convert.ToByte(random.Next(255));
+            BufferedStream bs = new BufferedStream(fs, bufsize);
+            for (int i = 0; i < countPart; i++)
+                bs.Write(buffer, 0, (int)bufsize);
+            fs.Close();
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        static byte[] ReadBufferedStream(string filename)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            int countPart = 4; //number of parts
+            int bufsize = (int)(fs.Length / countPart);
+            byte[] buffer = new byte[fs.Length];
+            BufferedStream bs = new BufferedStream(fs, bufsize);
+            for (int i = 0; i < countPart; i++)
+                bs.Read(buffer, 0, (int)bufsize);
+            fs.Close();
+            return buffer;
+        }
+    }
+}
