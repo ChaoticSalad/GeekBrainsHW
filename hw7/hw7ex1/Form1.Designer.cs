b033d6e765369a72ebbe2f4f2c214ad0182297b7
ChaoticSalad <olerator97@gmail.com>
2020-08-17 14:59:14 +0300

    Added description

--- hw7/hw7ex1/Form1.Designer.cs
+++ hw7/hw7ex1/Form1.Designer.cs
@@ -153,6 +153,7 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
--- hw7/hw7ex2/Form1.cs
+++ hw7/hw7ex2/Form1.cs
@@ -7,7 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//Используя Windows Forms, разработать игру «Угадай число». Компьютер загадывает число от 1 до 100, 
+//    а человек пытается его угадать за минимальное число попыток.Компьютер говорит, больше или меньше загаданное число введенного.
+//a) Для ввода данных от человека используется элемент TextBox;
+//б) ** Реализовать отдельную форму c TextBox для ввода числа.
+//    Черников Олег
 namespace hw7ex2
 {
     public partial class Form1 : Form
