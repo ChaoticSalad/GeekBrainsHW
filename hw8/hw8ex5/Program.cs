231bcc730f447804e693e200bb4913647f96fa65
ChaoticSalad <olerator97@gmail.com>
2020-08-21 13:09:17 +0300

    Fixed hm8ex5
    Now serialization works as intended

--- hw8/hw8ex5/Program.cs
+++ hw8/hw8ex5/Program.cs
@@ -5,10 +5,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using System.Xml.Serialization;
 //**Написать программу - преобразователь из CSV в XML-файл с информацией о студентах (6 урок).
 //    Черников Олег
 namespace hw8ex5
 {
+    public class Student
+    {
+        public string lastName;
+        public string firstName;
+        public string university;
+        public string faculty;
+        public int course;
+        public string department;
+        public int group;
+        public string city;
+        public int age;
+        public Student(string firstName, string lastName, string university, string faculty, string department, int age, int course, int group, string city)
+        {
+            this.lastName = lastName;
+            this.firstName = firstName;
+            this.university = university;
+            this.faculty = faculty;
+            this.department = department;
+            this.age = age;
+            this.course = course;
+            this.group = group;
+            this.city = city;
+        }
+        
+        public Student()
+        {
+
+        }
+    }
+
     class Program
     {
         /// <summary>Magic code</summary>
@@ -16,14 +47,29 @@ namespace hw8ex5
         /// <param name="fileNameSave">Xml file name</param>
         static void Converter(string fileNameOpen, string fileNameSave)
         {
-            string[] lines = File.ReadAllLines(fileNameOpen);
-            string[] headers = { "Name", "Surname", "University", "Faculty", "Department", "Age", "Course", "Group", "City" };
+            List<Student> students = new List<Student>();
+            StreamReader sr = new StreamReader(fileNameOpen); 
+            while (!sr.EndOfStream)
+            {
+                try
+                {
+                    string[] s = sr.ReadLine().Split(',');
+                    students.Add(new Student(s[0], s[1], s[2], s[3], s[4], int.Parse(s[5]), int.Parse(s[6]), int.Parse(s[7]), s[8]));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Error! ESC - to stop");
+                    if (Console.ReadKey().Key == ConsoleKey.Escape) 
+                        return;
+                }
+            }
+            sr.Close();
 
-            var xml = new XElement("Students",
-               lines.Where((line, index) => index > 0).Select(line => new XElement("StudentIndo",
-                  line.Split(';').Select((column, index) => new XElement(headers[index], column)))));
-
-            xml.Save(fileNameSave);
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(List<Student>)); 
+            Stream fStream = new FileStream(fileNameSave, FileMode.Create, FileAccess.Write);
+            xmlFormat.Serialize(fStream, students);
+            fStream.Close();
         }
 
         static void Main(string[] args)
